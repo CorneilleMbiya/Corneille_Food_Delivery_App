419e0f17c7492cd99aeb27e3dc1557e9bdfff7d0
CorneilleMbiya <nbmbiya@gmail.com>
2024-10-27 11:26:21 -0400

    updating

--- Front_End/src/pages/Home/Home.jsx
+++ Front_End/src/pages/Home/Home.jsx
@@ -2,7 +2,8 @@ import React, {useState} from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
-import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+import AppDownload from '../../components/AppDownload/AppDownload'
 
 
 function Home() {
@@ -12,6 +13,7 @@ function Home() {
         <Header/>
         <ExploreMenu category={category} setCategory={setCategory}/>
         <FoodDisplay category={category}/>
+        <AppDownload/>
     </div>
   )
 }
